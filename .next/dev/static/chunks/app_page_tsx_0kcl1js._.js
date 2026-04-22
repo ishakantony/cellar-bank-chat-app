1c771360d6f1128e427941f0be744cd824dfaed2
Ishak Antony Darmawan <ishak.antony@yahoo.com>
2026-04-22 21:23:06 +0800

    chore: ignore build artifacts and test results

--- .next/dev/static/chunks/app_page_tsx_0kcl1js._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0rzbs87._.js"],
-    source: "dynamic"
-});
