9034cbb14bb2ab314433a4510c0092d4ceaf1365
Ishak Antony Darmawan <ishak.antony@yahoo.com>
2026-04-27 11:59:05 +0800

    add hooks for pre-commit and pre-push

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+
+const eslintConfig = [
+  {
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "public/**",
+      "test-results/**",
+      ".worktrees/**",
+      "next-env.d.ts",
+    ],
+  },
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react-hooks/set-state-in-effect": "warn",
+      "react-hooks/error-boundaries": "warn",
+    },
+  },
+];
+
+export default eslintConfig;
